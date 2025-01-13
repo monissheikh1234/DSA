3d8a52f451631d82cd1914efccf5eb91a55c17f4
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-01-14 01:28:30 +0530

    Time: 0 ms (100%), Space: 11.8 MB (59.6%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string curr="";
+        string max="";
+        sort(strs.begin(),strs.end());
+        int n=strs.size();
+        int i=0;
+        while(i<strs[0].size() &&    strs[0][i]==strs[n-1][i]){
+            max+=strs[0][i];
+            i++;
+        }
+        
+        return max;
+
+    }
+};
