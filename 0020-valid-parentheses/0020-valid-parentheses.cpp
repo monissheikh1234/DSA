a1469f5a8ff1164b571ace8c8114e2f5d464775b
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-04-16 09:50:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (35.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> s1;
-        for (char &c : s) {
+        for (char c : s) {
             if (c == '(' || c == '[' || c == '{') s1.push(c);
             else {
                 if (s1.empty()) return false;
