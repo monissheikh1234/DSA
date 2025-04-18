46caffdba0d15ad19289b4cb9c07e4df9fcd0b67
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-04-18 19:26:00 +0530

    Time: 313 ms (19.11%), Space: 26.2 MB (36.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,52 +1,39 @@
 class Solution {
+private: 
+    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
+        if(i == j){
+            return dp[i][j] = true;
+        }
+        if(j-i == 1){
+            if(s[i] == s[j]){
+                return dp[i][j] = true;
+            }
+            else{
+                return dp[i][j] = false;
+            }
+        }
+        if(s[i] == s[j] && dp[i+1][j-1] == true){
+            return dp[i][j] = true;
+        } else {
+            return dp[i][j] = false;
+        }
+    }
 public:
-// Helper function to expand around the center
-string expandAroundCenter(const string& s, int left, int right) {
-    while (left >= 0 && right < s.size() && s[left] == s[right]) {
-        left--;
-        right++;
-    }
-    return s.substr(left + 1, right - left - 1);
-}
     string longestPalindrome(string s) {
-          if (s.empty()) return "";
-    
-    string longest = "";
-    for (int i = 0; i < s.size(); i++) {
-        // Odd-length palindrome
-        string oddPalindrome = expandAroundCenter(s, i, i);
-        if (oddPalindrome.size() > longest.size()) {
-            longest = oddPalindrome;
+        int n = s.size();
+        int startIndex = 0; int maxlen = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int g=0; g<n; g++){
+            for(int i=0, j=g; j<n; i++, j++){
+                solve(dp, i, j, s);
+                if(dp[i][j] == true){
+                    if(j-i+1 > maxlen){
+                        startIndex = i;
+                        maxlen = j-i+1;
+                    }
+                }
+            }
         }
-        
-        // Even-length palindrome
-        string evenPalindrome = expandAroundCenter(s, i, i + 1);
-        if (evenPalindrome.size() > longest.size()) {
-            longest = evenPalindrome;
-        }
-    }
-    return longest;
-    //     string result;
-    //     int max=0;
-    //     int count=0;
-    //     for(int i=0;i<s.size();i++){
-    //      for(int j=s.size()-1;s>=0;s++){
-    //         if(s[j]==s[i]){
-    //         left=i;right=j;
-    //        while(left<=right){
-    //    if(s[left]!=s[right]){
-    //     break;
-    //    }else{
-    //     currstring.push_back=s[left];
-    //     count=currstring.size();
-    //     maxcount=max(count,max);
-    //     if(count>max){
-    //         result.
-    //     }
-    //    }
-    //        }
-    //         }
-    //      }
-    //     }
+        return s.substr(startIndex, maxlen);
     }
 };
