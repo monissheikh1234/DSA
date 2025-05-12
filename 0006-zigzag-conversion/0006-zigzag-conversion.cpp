3ae59031a64d541a3d685abefb16b046e7382b53
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-05-12 23:13:03 +0530

    Time: 9 ms (36.68%), Space: 14.2 MB (37.33%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1) return s;
-        char a[10000];
-        std::size_t len = s.length();
-        int index =0;
-        for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < len; j +=2*(numRows-1)) {
-                a[index++] = s[j];
-                if (i > 0 && i<numRows-1 && j+2*(numRows-1)-2*i<len) {
-                    a[index++] = s[j + 2*(numRows - 1)-2*i];
-                }
-            }
-        }
-        a[index]='\0';
-        return a;
+        if(numRows==1) return s;
+       vector<string>pattern(min(numRows,int(s.size())));
+       int cycle = 2*numRows-2;
+
+       for(int i=0;i<s.size();i++){
+        int pos = i % cycle;
+        int row = (pos < numRows) ? pos : cycle-pos;
+        pattern[row] += s[i];
+       } 
+       string ans;
+       for(string row : pattern) ans += row;
+       return ans;
     }
 };
