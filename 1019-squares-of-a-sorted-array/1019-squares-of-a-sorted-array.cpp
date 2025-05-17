7605541a19740d31b6920739b95d4a3843a82fe7
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-05-17 17:36:38 +0530

    Time: 0 ms (100%), Space: 30.3 MB (63.31%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int left = 0;
-        int right = nums.size() - 1;
-        vector<int> result(nums.size());
-        int pos = nums.size() - 1;
+          int left=0;
+      int right=nums.size()-1;
+      int pos=nums.size()-1;
+      vector<int>result(nums.size());
+    
+     for(int i=0;i<nums.size();i++){
+        nums[i]=nums[i]*nums[i];
+     }
+  while(left<=right){
+    if(nums[left]>=nums[right]){
+        result[pos]=nums[left];
+        left++;
         
-        // Square each element
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = nums[i] * nums[i];
-        }
+    }
+    else{
+        result[pos]=nums[right];
+        right--;
+    }
+    pos--;
+  }
 
-        // Use two-pointer approach to sort the squared elements
-        while(left <= right){
-            if(nums[left] > nums[right]){
-                result[pos] = nums[left];
-                left++;
-            } else {
-                result[pos] = nums[right];
-                right--;
-            }
-            pos--;
-        }
+return result;
 
-        return result;
+
     }
 };
