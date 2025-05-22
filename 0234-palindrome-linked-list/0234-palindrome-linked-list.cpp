1eb8b005cd235700326f0fafcf84648f257eefc5
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-05-22 22:57:57 +0530

    Time: 69 ms (5.12%), Space: 152.5 MB (5.2%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> listVals;
-        while (head) {
-            listVals.push_back(head->val);
+        // Some Brute Force Steps to follow
+        // 1) Reverse the list and store it in r_head;
+        // and then compare head->val and r_head->val if equal move both the pointers otherwise if not equal return false.
+        // When the loop will be executed fully retrun true
+         if(head == NULL || head->next == NULL){
+            return (head);
+        }
+        ListNode *r_head = NULL;
+        ListNode *ptr = head;
+        while(ptr!=NULL){
+            ListNode *temp = new ListNode(ptr->val);
+            temp ->next = r_head;
+            r_head = temp;
+            ptr = ptr->next;
+        }
+        while(head && r_head){
+            if(head->val != r_head->val){
+                return false;
+            }
             head = head->next;
+            r_head = r_head->next;
         }
-        
-        int left = 0, right = listVals.size() - 1;
-        while (left < right && listVals[left] == listVals[right]) {
-            left++;
-            right--;
-        }
-        return left >= right;
+        return true;
     }
 };
