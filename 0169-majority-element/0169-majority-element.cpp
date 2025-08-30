15e177a30067a1e8106088ffc6f6d6f0f8025028
Monis Sheikh <158022523+monissheikh1234@users.noreply.github.com>
2025-08-30 15:55:57 +0530

    Time: 1 ms (37.16%), Space: 28.2 MB (62.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,32 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> mp; 
+//         int n = nums.size() / 2; 
+
+        
+//         for (auto a : nums) {
+//             mp[a]++;
+//         }
+
+//         int ans = 0; 
+
+        
+//         for (auto a : mp) {
+//             if (a.second > n) {
+//                 ans = a.first; 
+//             }
+//         }
+
+//         return ans; 
+//     }
+// };
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp; 
-        int n = nums.size() / 2; 
-
-        
-        for (auto a : nums) {
-            mp[a]++;
-        }
-
-        int ans = 0; 
-
-        
-        for (auto a : mp) {
-            if (a.second > n) {
-                ans = a.first; 
-            }
-        }
-
-        return ans; 
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n/2];
     }
 };
-
 
